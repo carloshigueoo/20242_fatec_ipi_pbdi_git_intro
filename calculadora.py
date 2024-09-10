e97de96508230defb96155fe9359f0436a4882d5
Carlos Kinjo <carloshigueoo@gmail.com>
2024-09-10 10:35:29 -0300

    feat: funcao e teste somar, subtrair, multiplicar implementado

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+def somar(a, b):
+  return a + b
+
+def subtrair(a, b):
+  return somar(a, b * -1)
+
+def multiplicacao (a,b):
+  return a*b
---
+++ teste_calculadora.py
@@ -0,0 +1,4 @@
+import calculadora
+print(calculadora.somar(1, 2))
+print(calculadora.subtrair(3, 2))
+print(calculadora.multiplicacao(2,1))
